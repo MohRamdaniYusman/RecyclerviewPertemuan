1e6912ff24b27df21158d0ed1fb0f664540f428e
Moh Ramdani Yusman <moh.ramdani22@gmail.com>
2019-11-14 08:38:54 +0700

    GET API

---
+++ app/src/main/java/com/example/recyclerviewpertemuan/Constans.java
@@ -0,0 +1,9 @@
+package com.example.recyclerviewpertemuan;
+
+public class Constans {
+    //base url
+    public static final String WEB_URL = "https://api.themoviedb.org/3";
+    //get now playing
+    public static final String GET_NOW_PLAYING = WEB_URL +
+            "/movie/now_playing?api_key=fdf08b0280f37c4d4b8b25b2089564ab";
+}
